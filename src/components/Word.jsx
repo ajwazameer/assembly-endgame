e25460c3f86cae0b080d335828c52cdf0c0fe440
ajwazameer <ajwazameer2004@gmail.com>
2026-08-02 16:00:30 +0500

    Create responsive layout for word box

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,1 +1,14 @@
-export default function Word() {}
+export default function Word() {
+  return (
+    <section className="word-box">
+      <div className="word-divs">H</div>
+      <div className="word-divs">H</div>
+      <div className="word-divs">H</div>
+      <div className="word-divs">H</div>
+      <div className="word-divs">H</div>
+      <div className="word-divs">H</div>
+      <div className="word-divs">H</div>
+      <div className="word-divs">H</div>
+    </section>
+  );
+}
